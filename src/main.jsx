717d45e30691e040d22627604febb2c0a450128c
agamshakir1234-code <agamshakir1234@gmail.com>
2026-03-31 22:47:16 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-
-// Providers
 import { AuthProvider } from './context/AuthContext.jsx'
 import { ToastProvider } from './context/ToastContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
-
-// Styles
 import './styles/globals.css'
-
-// Demo data init
 import { initDemo } from '@/services/demoData'
 
-// Initialize demo data once
 initDemo()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
