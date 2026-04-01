432bd146594a9bcbba8b2b724bb636f4f442651b
agamshakir1234-code <agamshakir1234@gmail.com>
2026-04-01 12:10:55 +0300

    Update index.js

--- src/components/leads/index.js
+++ src/components/leads/index.js
@@ -1,1 +1,5 @@
+export { default as LeadModal } from "./LeadModal"
+export { default as LeadCreateModal } from "./LeadCreateModal"
+export { default as LeadEditModal } from "./LeadEditModal"
+export { default as LeadViewModal } from "./LeadViewModal"
 
