640cb651f58b4b6919b018dc38492ee819eb7d45
agamshakir1234-code <agamshakir1234@gmail.com>
2026-04-01 12:13:38 +0300

    Update Leads.jsx

--- src/pages/Leads.jsx
+++ src/pages/Leads.jsx
@@ -1,4 +1,37 @@
-export { default as LeadModal } from "./LeadModal"
-export { default as LeadCreateModal } from "./LeadCreateModal"
-export { default as LeadEditModal } from "./LeadEditModal"
-export { default as LeadViewModal } from "./LeadViewModal"
+export default function Leads() {
+  const [createOpen, setCreateOpen] = useState(false)
+  const [editOpen, setEditOpen] = useState(false)
+  const [viewOpen, setViewOpen] = useState(false)
+
+  const [selectedLead, setSelectedLead] = useState(null)
+
+  return (
+    <div className="space-y-5">
+
+      {/* כל התוכן של עמוד הלידים שלך כאן */}
+
+      {/* מודל יצירת ליד */}
+      <LeadCreateModal
+        open={createOpen}
+        onClose={() => setCreateOpen(false)}
+        onCreate={addLead}
+      />
+
+      {/* מודל עריכת ליד */}
+      <LeadEditModal
+        open={editOpen}
+        onClose={() => setEditOpen(false)}
+        lead={selectedLead}
+        onSave={saveLead}
+      />
+
+      {/* מודל צפייה בליד */}
+      <LeadViewModal
+        open={viewOpen}
+        onClose={() => setViewOpen(false)}
+        lead={selectedLead}
+      />
+
+    </div>
+  )
+}
